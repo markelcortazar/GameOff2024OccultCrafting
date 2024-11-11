c76076475a2826efa973a153e53500052a47f84d
markelcortazar <markelcortazar@gmail.com>
2024-11-11 10:33:39 +0100

    Fixed Grab Manager to mantain previous Grabbable functionality

--- GameOff-OccultCrafter/Assets/Scripts/GrabManager.cs
+++ GameOff-OccultCrafter/Assets/Scripts/GrabManager.cs
@@ -13,41 +13,60 @@ public class GrabManager : MonoBehaviour
     public void Grab(InputAction.CallbackContext context)
     {
         if (context.started)
-        {
-            //Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            //Ray ray = new Ray(cam.transform.position, Vector3.Reflect(cam.ScreenToWorldPoint(Input.mousePosition).normalized, Vector3.forward));
-            //Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-            //RaycastHit hit;
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), Vector2.zero);
-            if ( hit.collider != null)
-            {
-                if (hit.collider.gameObject.CompareTag("Grabbable"))
-                    Debug.Log("Cowagunga it is " + hit.collider.gameObject.name);
-
-            }
-            else Debug.Log("Waaaaaaah . . .");
-        }
-
+            TryFindAndGrabObject();
+        else if (context.canceled)
+            TryReleaseObject();
     }
-        //    Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        //    mousePos.z = 0;
-        //    if (boundsCollider.bounds.Contains(mousePos))
-        //    {
-        //        isGrabbed = true;
-        //        boundsCollider.enabled = false;
-        //    }
-        //}
-        //else if (context.canceled && isGrabbed)
-        //{
-        //    boundsCollider.enabled = true;
-        //    isGrabbed = false;
 
-        //}
+    private void TryReleaseObject()
+    {
+        if (grabbedElement == null) return;
+        grabbedElement.GetComponent<Collider2D>().enabled = true;
+        grabbedElement = null;
+    }
+
+    private void TryFollowMouseWhilePressed()
+    {
+        if (grabbedElement == null) return;
+        Vector3 newPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        newPos.z = grabbedElement.position.z;
+        Debug.Log(newPos);
+        grabbedElement.position = newPos;
+    }
+
+    private void TryFindAndGrabObject()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), Vector2.zero);
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.CompareTag("Grabbable"))
+            {
+                grabbedElement = hit.collider.gameObject.transform;
+                grabbedElement.gameObject.GetComponent<Collider2D>().enabled = false;
+            }
+
+        }
+    }
+
+    //    Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    //    mousePos.z = 0;
+    //    if (boundsCollider.bounds.Contains(mousePos))
+    //    {
+    //        isGrabbed = true;
+    //        boundsCollider.enabled = false;
+    //    }
+    //}
+    //else if (context.canceled && isGrabbed)
+    //{
+    //    boundsCollider.enabled = true;
+    //    isGrabbed = false;
+
+    //}
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        TryFollowMouseWhilePressed();
     }
 }
